e80bd9d0da58140f7074fe51eae3315553371285
Natzaum <natacezerbordignon@gmail.com>
2024-10-31 16:00:30 -0300

    bug: stock id reajusted for the shoppingcart

--- src/model/DAO/StockDao.java
+++ src/model/DAO/StockDao.java
@@ -5,6 +5,7 @@ import Store.Stock;
 public interface StockDao {
     void insert(Stock obj);
     void read(Stock obj);
+    Stock findByName(String name);
     void update(Stock obj);
     void delete(Stock obj);
 }
